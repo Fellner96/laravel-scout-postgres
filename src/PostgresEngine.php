ec1c20bf2e2f249ffad6bc66762607126c81863a
Fellner <fellner@gmail.com>
2019-11-14 09:33:41 +0100

    fix

--- src/PostgresEngine.php
+++ src/PostgresEngine.php
@@ -120,6 +120,7 @@ class PostgresEngine extends Engine
         // file or individually for each model in searchableOptions()
         // See https://www.postgresql.org/docs/current/static/textsearch-controls.html
         $vector = 'to_tsvector(COALESCE(?, get_current_ts_config()), ?)';
+        dump('nocunnent');
         if ($this->config('unaccent') === true) {
             dump('unaccent');
             $vector = 'to_tsvector(COALESCE(?, get_current_ts_config()), unaccent(?))';
